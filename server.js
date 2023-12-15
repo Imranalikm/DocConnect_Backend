65609b52a46bf9e012e23f6f13d3221fe86d38e2
Imranalikm <imranalikm@gmail.com>
2023-12-15 21:01:43 +0530

    origin error

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.resolve() + "/public"))
 app.use(
   cors({
-    origin: 'http://localhost:3001,https://docconnect-six.vercel.app',
+    origin: 'https://docconnect-six.vercel.app',
     credentials: true, // Allow sending cookies with the request
   })
 );
